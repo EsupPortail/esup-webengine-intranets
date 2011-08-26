1f187bafc4e09dd42ee039a9a0d2f52716027e78
bourges <bourges@2bebc3db-464a-0410-8034-b528f6c09253>
2011-08-26 14:58:08 +0000

    un peu de mise en forme et références à nuxeo 5.4.1
    git-svn-id: https://subversion.renater.fr/esup-ecm/intranets/trunk/esup-webengine-intranets@676 2bebc3db-464a-0410-8034-b528f6c09253

--- README.txt
+++ README.txt
@@ -1,16 +1,16 @@
 Le projet esup-webengine-intranets est un module Nuxeo WebEngine.
 
-Le projet esup-webengine-intranets propose de présenter une section
-d'esup-ecm à la manière d'un intranet.
+Le projet esup-webengine-intranets propose de prÃ©senter une section
+d'esup-ecm Ã  la maniÃ¨re d'un intranet.
 
 Il permet de :
 
 - naviguer dans les sous-dossiers et les fichiers de la section
 - rechercher un document dans la section
 - d'afficher l'arborescence de la section
-- afficher les derniers documents modifiés ou ajoutés
+- afficher les derniers documents modifiÃ©s ou ajoutÃ©s
 
-La section est définie dans le fichier "src/main/resources/OSGI-INF/intranets.properties"
+La section est dÃ©finie dans le fichier "src/main/resources/OSGI-INF/intranets.properties"
 par ex :
 
 section.path=/default-domain/sections/intranets/
--- src/main/java/org/esupportail/ecm/intranets/Main.java
+++ src/main/java/org/esupportail/ecm/intranets/Main.java
@@ -31,36 +31,34 @@ import org.nuxeo.ecm.webengine.model.*;
 import org.nuxeo.ecm.webengine.model.exceptions.*;
 import org.nuxeo.ecm.webengine.model.impl.*;
 
-
-
 @WebObject(type = "esupintranets")
 @Produces("text/html; charset=UTF-8")
 public class Main extends ModuleRoot {
 
 	public static final Log log = LogFactory.getLog(Main.class);
 	protected String sectionPath;
-	
+
 	public Main() {
-    	    Map<Object, Object> intranetsProperties = new HashMap<Object, Object>();
-    	    Properties p = new Properties();
-            URL url = Main.class.getClassLoader().getResource(
-	            "OSGI-INF/esup-intranets.properties");
-    	    InputStream in = null;
-            try {
-	        in = url.openStream();
-    	        p.load(in);
-        	intranetsProperties.putAll(p);
-            } catch (IOException e) {
-	        throw new Error("Failed to load mime types");
-    	    } finally {
-        	if (in != null) {
-            	    try {
-                	in.close();
-                    } catch (Exception e) {
-	            }
-    	        }
-    	    }
-            sectionPath = (String) intranetsProperties.get("section.path");
+		Map<Object, Object> intranetsProperties = new HashMap<Object, Object>();
+		Properties p = new Properties();
+		URL url = Main.class.getClassLoader().getResource(
+				"OSGI-INF/esup-intranets.properties");
+		InputStream in = null;
+		try {
+			in = url.openStream();
+			p.load(in);
+			intranetsProperties.putAll(p);
+		} catch (IOException e) {
+			throw new Error("Failed to load mime types");
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (Exception e) {
+				}
+			}
+		}
+		sectionPath = (String) intranetsProperties.get("section.path");
 	}	
 
 	/**
@@ -68,84 +66,72 @@ public class Main extends ModuleRoot {
 	 */
 	@Path("repository")
 	public Object getRepository() {
-	
-	    ctx.setProperty("sectionPath", sectionPath);
-	    return new DocumentRoot(ctx, sectionPath);
-	    
+		ctx.setProperty("sectionPath", sectionPath);
+		return new DocumentRoot(ctx, sectionPath);
 	}
-	
+
 	/**
 	 *	Get file
 	 */
 	@GET
 	@Path("file/{path:.*}")
 	public Object getFile(@PathParam("path") String path) 
-		throws PropertyException, ClientException {
-		
-	    Object requestedObject;
-	    Property propertyFile = null;
-	    String requestedFilename = "";
-	    Blob requestedBlob = null;
-	    path = sectionPath + path;
-	    CoreSession session = ctx.getCoreSession();
-	    DocumentRef docRef = new PathRef(path);
-	    DocumentModel doc = null;
-	    //DocumentModel versionDoc = null;
-	    
-	    ctx.setProperty("sectionPath", sectionPath);
-	    
-	    log.error("getFile");
-	    log.error("path :: " + path);
-	    
-	    //doc = session.getDocument(docRef);
-	    //propertyFile = doc.getProperty("file:content");
-	    
-	    DocumentModelList proxies = session.getProxies(docRef, null);
-	    if (proxies.size() > 0)
-            doc = proxies.get(0);
-	    else
-	    	doc = session.getDocument(docRef);
-	    
-	    
-	    propertyFile = doc.getProperty("file:content");
-	    
-	    if (propertyFile != null) {
-		    requestedBlob = (Blob) propertyFile.getValue();
-		if (requestedBlob == null) {
-		    throw new WebResourceNotFoundException(
-			"No attached file at " + "file:content");
+			throws PropertyException, ClientException {
+
+		Object requestedObject;
+		Property propertyFile = null;
+		String requestedFilename = "";
+		Blob requestedBlob = null;
+		path = sectionPath + path;
+		CoreSession session = ctx.getCoreSession();
+		DocumentRef docRef = new PathRef(path);
+		DocumentModel doc = null;
+		//DocumentModel versionDoc = null;
+		ctx.setProperty("sectionPath", sectionPath);
+		log.error("getFile");
+		log.error("path :: " + path);
+		//doc = session.getDocument(docRef);
+		//propertyFile = doc.getProperty("file:content");
+		DocumentModelList proxies = session.getProxies(docRef, null);
+		if (proxies.size() > 0) {
+			doc = proxies.get(0);
 		}
-		requestedFilename = requestedBlob.getFilename();
-		if (requestedFilename == null) {
-		    propertyFile = propertyFile.getParent();
-		    if (propertyFile.isComplex()) {
-			try {
-			    requestedFilename = (String) propertyFile
-				.getValue("filename");
-			} catch (PropertyException e) {
-			    requestedFilename = "Unknown";
+		else {
+			doc = session.getDocument(docRef);			
+		}
+		propertyFile = doc.getProperty("file:content");
+		if (propertyFile != null) {
+			requestedBlob = (Blob) propertyFile.getValue();
+			if (requestedBlob == null) {
+				throw new WebResourceNotFoundException(
+						"No attached file at " + "file:content");
 			}
-		    }
+			requestedFilename = requestedBlob.getFilename();
+			if (requestedFilename == null) {
+				propertyFile = propertyFile.getParent();
+				if (propertyFile.isComplex()) {
+					try {
+						requestedFilename = (String) propertyFile
+								.getValue("filename");
+					} catch (PropertyException e) {
+						requestedFilename = "Unknown";
+					}
+				}
+			}
+			requestedObject = Response.ok(requestedBlob).header(
+					"Content-Disposition",
+					"inline; filename=" + requestedFilename).type(
+							requestedBlob.getMimeType()).build();
+			return requestedObject;
 		}
-		requestedObject = Response.ok(requestedBlob).header(
-				    "Content-Disposition",
-				    "inline; filename=" + requestedFilename).type(
-					requestedBlob.getMimeType()).build();
-		return requestedObject;
-	    }
-	    
-	    return this;
-	    
+		return this;
 	}
-	
-	
 
 	/**
 	 * @see org.nuxeo.ecm.webengine.model.impl.ModuleRoot#handleError(javax.ws.rs.WebApplicationException)
 	 */
 	@Override
 	public Object handleError(WebApplicationException e) {
-	
 		if (e instanceof WebSecurityException) {
 			//forge a cookie in order to come back after authenticate
 			CoreSession coreSession = ctx.getCoreSession();
@@ -174,38 +160,26 @@ public class Main extends ModuleRoot {
 				Response requestedObject = responseBuilder.build();
 				return requestedObject;
 			}
-
 		}
 		return super.handleError(e);
-		
 	}
-
-
 
 	/**
 	 * Default view
 	 */
 	@GET
 	public Object doGet() {
-	
 		return getView("index");
-		
 	}
 
-
-	
 	/**
 	 * search form
 	 */
 	@GET
 	@Path("form")
 	public Object getViewForm() {
-	
-	    return getView("form");
-	    
+		return getView("form");
 	}
-
-
 
 	/**
 	 *	Get tree view
@@ -213,13 +187,9 @@ public class Main extends ModuleRoot {
 	@GET
 	@Path("tree")
 	public Object getViewTree() {
-
-	    ctx.setProperty("sectionPath", sectionPath);
-	    return getView("tree").arg("doc", new DocumentRoot(ctx, sectionPath));
-	    
+		ctx.setProperty("sectionPath", sectionPath);
+		return getView("tree").arg("doc", new DocumentRoot(ctx, sectionPath));
 	}
-
-
 
 	/**
 	 *	Search result
@@ -227,75 +197,61 @@ public class Main extends ModuleRoot {
 	@GET
 	@Path("@search")
 	public Object search() {
-	
-	    ctx.setProperty("sectionPath", sectionPath);
-    	    String query = ctx.getRequest().getParameter("query");
-    	    if (query == null) {
-        	String fullText = ctx.getRequest().getParameter("fullText");
-        	if (fullText == null) {
-            	    throw new IllegalParameterException("Expecting a query or a fullText parameter");
-        	}
-        	String orderBy = ctx.getRequest().getParameter("orderBy");
-        	String orderClause = "";
-        	if (orderBy != null) {
-            	    orderClause = " ORDER BY " + orderBy;
-        	}
-        	query = "SELECT * FROM Document WHERE (ecm:fulltext = \"" + fullText
-                    + "\") AND (ecm:isCheckedInVersion = 0) AND (ecm:path STARTSWITH \"" + sectionPath + "\")" + orderClause;
-    	    }
-    	    try {
-        	DocumentModelList docs = ctx.getCoreSession().query(query);
-        	return getView("search").arg("query", query).arg("result", docs);
-    	    } catch (ClientException e) {
-        	throw WebException.wrap(e);
-    	    }
-    	    
+		ctx.setProperty("sectionPath", sectionPath);
+		String query = ctx.getRequest().getParameter("query");
+		if (query == null) {
+			String fullText = ctx.getRequest().getParameter("fullText");
+			if (fullText == null) {
+				throw new IllegalParameterException("Expecting a query or a fullText parameter");
+			}
+			String orderBy = ctx.getRequest().getParameter("orderBy");
+			String orderClause = "";
+			if (orderBy != null) {
+				orderClause = " ORDER BY " + orderBy;
+			}
+			query = "SELECT * FROM Document WHERE (ecm:fulltext = \"" + fullText
+					+ "\") AND (ecm:isCheckedInVersion = 0) AND (ecm:path STARTSWITH \"" + sectionPath + "\")" + orderClause;
+		}
+		try {
+			DocumentModelList docs = ctx.getCoreSession().query(query);
+			return getView("search").arg("query", query).arg("result", docs);
+		} catch (ClientException e) {
+			throw WebException.wrap(e);
+		}
 	}
-	
-	
-	
+
 	/**
-     *      get latests documents
-     */
-    @GET
-    @Path("news")
-    public Object getNews() {
+	 *      get latests documents
+	 */
+	@GET
+	@Path("news")
+	public Object getNews() {
+		ctx.setProperty("sectionPath", sectionPath);
+		String query = "SELECT * FROM Document WHERE (ecm:path STARTSWITH \"" + sectionPath + "\")"
+				+ " AND (ecm:primaryType = 'File') ORDER BY dc:modified DESC ";
+		try {
+			DocumentModelList docs = ctx.getCoreSession().query(query, 10);
+			return getView("news").arg("query", query).arg("result", docs);
+		} catch (ClientException e) {
+			throw WebException.wrap(e);
+		}
+	}
 
-        ctx.setProperty("sectionPath", sectionPath);
-        String query = "SELECT * FROM Document WHERE (ecm:path STARTSWITH \"" + sectionPath + "\")"
-             + " AND (ecm:primaryType = 'File') ORDER BY dc:modified DESC ";
-
-        try {
-            DocumentModelList docs = ctx.getCoreSession().query(query, 10);
-            return getView("news").arg("query", query).arg("result", docs);
-        } catch (ClientException e) {
-            throw WebException.wrap(e);
-        }
-
-    }
-    
-    
-    
-
-    /**
-     *      rss feed
-     */
-    @GET
-    @Path("rss")
-    public Object getRSS() {
-
-        ctx.setProperty("sectionPath", sectionPath);
-        String query = "SELECT * FROM Document WHERE (ecm:path STARTSWITH \"" + sectionPath + "\")"
-             + " AND (ecm:primaryType = 'File') ORDER BY dc:modified DESC ";
-
-        try {
-            DocumentModelList docs = ctx.getCoreSession().query(query, 10);
-            return getView("rss").arg("query", query).arg("result", docs);
-        } catch (ClientException e) {
-            throw WebException.wrap(e);
-        }
-
-    }
-
+	/**
+	 *      rss feed
+	 */
+	@GET
+	@Path("rss")
+	public Object getRSS() {
+		ctx.setProperty("sectionPath", sectionPath);
+		String query = "SELECT * FROM Document WHERE (ecm:path STARTSWITH \"" + sectionPath + "\")"
+				+ " AND (ecm:primaryType = 'File') ORDER BY dc:modified DESC ";
+		try {
+			DocumentModelList docs = ctx.getCoreSession().query(query, 10);
+			return getView("rss").arg("query", query).arg("result", docs);
+		} catch (ClientException e) {
+			throw WebException.wrap(e);
+		}
+	}
 }
 
